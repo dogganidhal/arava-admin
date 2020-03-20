f99ceffb101229612a68c416ef3430e04c50220d
DOGGA Nidhal <dogga.nidhal@gmail.com>
2020-03-20 16:55:22 +0100

    Re-work poi list with filters and stuff

--- src/Components/PoiList.tsx
+++ src/Components/PoiList.tsx
@@ -1,23 +1,21 @@
 import React, {useCallback, useEffect, useState} from "react";
 import {
-	Button, createStyles, Fab,
-	IconButton, InputBase, Paper,
-	Table, TableBody, TableCell,
-	TableHead, TableRow, Theme
+	createStyles, Fab, IconButton,
+	TableCell, TableRow, Theme
 } from "@material-ui/core";
 import usePoiListService from "../Hooks/UsePoiListService";
 import AppLoader from "./AppLoader";
 import Alert from "@material-ui/lab/Alert";
 import {makeStyles} from "@material-ui/core/styles";
-import AddIcon from '@material-ui/icons/Search';
-import SearchIcon from '@material-ui/icons/Add';
 import {useHistory} from "react-router-dom";
 import usePoiFilter from "../Hooks/UsePoiFilter";
 import CheckIcon from "@material-ui/icons/Check";
 import CancelIcon from "@material-ui/icons/Close";
+import SearchIcon from '@material-ui/icons/Add';
 import Poi from "../Data/Model/Poi";
 import useIoC from "../Hooks/UseIoC";
 import PoiService from "../Data/Service/Poi/PoiService";
+import MUIDataTable from "mui-datatables";
 
 const useStyles = makeStyles((theme: Theme) =>
 	createStyles({
@@ -49,9 +47,10 @@ const useStyles = makeStyles((theme: Theme) =>
 			padding: theme.spacing(2),
 		},
 		fab: {
-			position: 'absolute',
+			position: 'fixed',
 			right: theme.spacing(2),
 			bottom: theme.spacing(2),
+			zIndex: 999
 		}
 	}),
 );
@@ -90,6 +89,19 @@ export default function PoiList() {
 		}
 	}, [allPois]);
 
+	const mapToTableData = useCallback(() => {
+		return pois.map(poi => {
+			return [
+				poi.title.find(t => t.language.code === 'fr')?.resource,
+				poi.theme.name.find(t => t.language.code === 'fr')?.resource,
+				poi.island.name,
+				poi.draft ? "oui" : "non",
+				poi.sponsored ? "oui" : "non",
+				poi.featured ? "oui" : "non"
+			];
+		});
+	}, [pois]);
+
 	if (isLoading) {
 		return <AppLoader />;
 	}
@@ -110,68 +122,50 @@ export default function PoiList() {
 			<SearchIcon />
 			Créer
 		</Fab>
-		<Paper variant={"outlined"} className={classes.searchPaper} component={"form"}>
-			<IconButton className={classes.iconButton} aria-label="menu">
-				<AddIcon />
-			</IconButton>
-			<InputBase
-				className={classes.input}
-				placeholder="Chercher des pois"
-				onChange={e => setQuery(e.target.value)}/>
-		</Paper>
-		<Paper variant={"outlined"} >
-			<Table className={classes.table} aria-label="simple table">
-				<TableHead>
-					<TableRow>
-						<TableCell>Titre</TableCell>
-						<TableCell align="center">Thème</TableCell>
-						<TableCell align="center">Île</TableCell>
-						<TableCell align="center">Brouillon</TableCell>
-						<TableCell align="center">Choses à faire</TableCell>
-						<TableCell align="center">Mis en avant</TableCell>
-						<TableCell align="center">Actions</TableCell>
-					</TableRow>
-				</TableHead>
-				<TableBody>
-					{allPois.map((poi, index) => (
-						<TableRow key={poi.id}>
-							<TableCell component="th" scope="row">
-								{poi.title.find(t => t.language.code === 'fr')?.resource}
-							</TableCell>
-							<TableCell align="center">{poi.theme.name.find(t => t.language.code === 'fr')?.resource}</TableCell>
-							<TableCell align="center">{poi.island.name}</TableCell>
-							<TableCell align="center">
+		<MUIDataTable
+			title={"Liste des points d'intérêt"}
+			data={mapToTableData()}
+			columns={["Titre", "Thème", "Île", "Brouillon", "Choses à faire", "Mis en avant"]}
+			options={{
+				filterType: "checkbox",
+				elevation: 0,
+				selectableRows: "none",
+				customRowRender: (data: any[], dataIndex: number, rowIndex: number) => {
+					return <TableRow>
+						{
+							[0, 1, 2].map(dataIndex => (
+								<TableCell component="th" scope="row">
+									{data[dataIndex]}
+								</TableCell>
+							))
+						}
+						{
+							<TableCell>
 								<IconButton
 									color={"primary"}
-									onClick={() => togglePoiDraft(poi, index)}>
+									onClick={() => togglePoiDraft(pois[rowIndex], rowIndex)}>
 									{
-										poi.draft ?
+										data[3] === "oui" ?
 											<CheckIcon /> :
 											<CancelIcon />
 									}
 								</IconButton>
 							</TableCell>
-							<TableCell align="center">
-								{
-									poi.sponsored ? <CheckIcon /> : <CancelIcon />
-								}
-							</TableCell>
-							<TableCell align="center">
-								{
-									poi.featured ? <CheckIcon /> : <CancelIcon />
-								}
-							</TableCell>
-							<TableCell align="center">
-								<Button
-									color={"primary"}
-									onClick={() => navigation.push(`/pois/${poi.id}`)}>
-									Modifier
-								</Button>
-							</TableCell>
-						</TableRow>
-					))}
-				</TableBody>
-			</Table>
-		</Paper>
+						}
+						{
+							[4, 5].map(dataIndex => (
+								<TableCell>
+									{
+										data[dataIndex] === "oui" ?
+											<CheckIcon /> :
+											<CancelIcon />
+									}
+								</TableCell>
+							))
+						}
+					</TableRow>;
+				}
+			}}
+		/>
 	</div>;
 }
